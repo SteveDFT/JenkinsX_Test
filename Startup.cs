c9c2268c4891677c8128a6046b7caaaf02ae8b5d
SteveDFT <steve.newman@dft.gov.uk>
2019-07-19 16:58:03 +0100

    test

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+// a comment
 namespace jenkinsTest
 {
     public class Startup
